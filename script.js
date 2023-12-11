c848730eedb9fb3744903ab164e96f31a6b65ded
Matthew Coleman <matthewcoleman0066@gmail.com>
2023-12-11 13:15:22 -0500

    Added a readme file, adjusted some comments

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
 let sidebar = document.getElementById("sidebar")
-//anonymous version of my scrolling event
 window.addEventListener("scroll", (event) => {
-    //when the user scrolls down 100 pixels, the sidebar is set to active
+    //when the user scrolls down 200 pixels, the sidebar is set to active
     //see the css file for more, but basically:
     //it's set to visible, and the fade in animation plays
     if (window.scrollY > 200) {
         sidebar.classList.add("active-sidebar")
     }
     //when the user scrolls back up, the fade out animation plays
-    //this only plays AFTER the fide-in has played already
+    //the end of this event is listened to (see line 19)
     if (window.scrollY < 201 && sidebar.classList.contains("active-sidebar")) {
         sidebar.style.animation="fade_out .5s"
     }
 })
 
-//once the fade-out animation plays, THEN it's set to invisible
-//this prevents the sidebare from vanishing instantly, since you cant set visibility in an animation
-//and obviously, instant invisibility renders the animation pointless
+//once the fade-out animation plays, THEN the menu is set to invisible
+//this prevents the sidebar from vanishing instantly
+//since instant invisibility cut off the animation
 document.addEventListener('animationend', function (e) {
     if (e.animationName === "fade_out") {
         e.target.classList.remove("active-sidebar");
         sidebar.style.removeProperty("animation")
     }
 });
-
-//traditional version of my scroll event, for if i want to call it elsewhere in the code
-/* window.addEventListener("scroll", scrollEvent)
-
-function scrollEvent(event) {
-    if (window.scrollY > 100) {
-        sidebar.classList.add("active-sidebar")
-        console.log(sidebar)
-    }
-    if (window.scrollY < 101 && sidebar.classList.contains("active-sidebar")) {
-        sidebar.style.animation="fade_out .5s"
-        //sidebar.classList.remove("active-sidebar")
-        console.log(sidebar)
-    }
-} */
 
